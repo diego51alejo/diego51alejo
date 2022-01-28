a807bfa668040bfe679bcba4ba40c0842b574ddd
Diego Munoz <diego51alejo@gmail.com>
2022-01-27 19:27:52 -0500

    Version 1.0

---
+++ app.js
@@ -0,0 +1,29 @@
+let name = document.getElementById('name')
+
+let counter = 0;
+
+// const lista = ['Muñoz', 'Ale', 'Camayo']
+
+// setInterval(() => {
+  
+//     for (let i in lista){
+//       name.innerHTML = lista[i]
+//       i += 1
+//     }
+  
+// }, 2000)
+
+
+// setInterval(() => {
+//   if(counter == 80){
+//     clearInterval()
+//   } else{
+//     counter += 1;
+//     number2.innerHTML = counter + '%'
+//   }
+  
+// }, 30)
+
+
+
+
